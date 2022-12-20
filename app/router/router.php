ea26bd41ad3659afa541777fa1090ecbeb792723
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2022-12-20 19:11:11 -0300

    teste

--- app/router/router.php
+++ app/router/router.php
@@ -1,24 +1,73 @@
 <?php
 
-function routes(){
- return require 'routes.php';
+function routes()
+{
+    return require 'routes.php';
 }
 
-function exactMatchUriInArrayRoutes($uri, $routes){
+function exactMatchUriInArrayRoutes($uri, $routes)
+{
 
     if (array_key_exists($uri, $routes)) {
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
 }
 
+function regularExpressionMatchArrayRoutes($uri, $routes)
+{
+    return array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+function params($uri, $matchedUri)
+{
+    if (!empty($matchedUri)) {
+        $matchedToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            $uri,
+            explode('/', ltrim($matchedToGetParams, '/'))
+        );
+    }
+    return [];
+}
+
+function paramsFormat($uri, $params){
+
+    $paramsData = [];
+    foreach ($params as $index => $param) {
+        $paramsData[$uri[$index - 1]] = $param;
+    }
+    return $paramsData;
+}
+
 function router()
 {
-   $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-   
-   $routes = routes();
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-   $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+    $routes = routes();
 
+    $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if (empty($matchedUri)) {
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+            $uri = explode('/',ltrim($uri, '/'));
+            $params = params($uri, $matchedUri);
+            $params = paramsFormat($uri, $params);
+
+            var_dump($params['user']);
+            die();
+        
+    }
+
+
+    var_dump($matchedUri);
+    die();
 }
