3de29710f2bbad73972c9eb7f4137e24236e5a61
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2023-01-02 19:46:44 -0300

    teste

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -9,38 +9,84 @@ class User
         if (!isset($params['user'])) {
             return redirect('/');
         }
+
         $user = findBy('users', 'id', $params['user']);
-        var_dump($user);
         die();
     }
 
-    public function create()
+
+    public function edit()
+    {
+        if (!logged()) {
+            redirect('/');
+        }
+
+        // read('users', 'users.id,firstName,lastName,email,password,path');
+        // tableJoin('photos', 'id', 'left');
+        // where('users.id', user()->id);
+
+        // $user = execute(isFetchAll: false);
+
+        // return [
+        //     'view'  => 'edit',
+        //     'data' => ['title' => 'Edit', 'user' => $user]
+        // ];
+    }
+
+    public function create(): array
     {
         return [
-            'view' => 'create',
+            'view'  => 'create',
             'data' => ['title' => 'Create']
         ];
     }
+
     public function store()
     {
         $validate = validate([
             'firstName' => 'required',
             'lastName' => 'required',
-            'email' => 'email|required|unique:users',
+            'email' => 'required|email|unique:users',
             'password' => 'maxlen:5|required',
-        ]);
+        ], persistInputs: true, checkCsrf: true);
+
         if (!$validate) {
             return redirect('/user/create');
         }
 
-
         $validate['password'] = password_hash($validate['password'], PASSWORD_DEFAULT);
-        //var_dump($validate);
+
         $created = create('users', $validate);
+
         if (!$created) {
             setFlash('message', 'Ocorreu um erro ao cadastrar, tente novamente em alguns segundo');
             return redirect('/user/create');
         }
+
         return redirect('/');
+    }
+
+    public function update($args)
+    {
+        if (!isset($args['user']) || $args['user'] !== user()->id) {
+            return redirect('/');
+        }
+
+        $validated = validate([
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email|uniqueUpdate:users,id=' . $args['user']
+        ]);
+
+        if (!$validated) {
+            return redirect('/user/edit/profile');
+        }
+
+        $updated = update('users', $validated, ['id' => user()->id]);
+
+        if ($updated) {
+            return setMessageAndRedirect('updated_success', 'Atualizado com sucesso', '/user/edit/profile');
+        }
+        setMessageAndRedirect('updated_error', 'Ocorreu um erro ao atualizar', '/user/edit/profile');
     }
 }
--- app/helpers/validate.php
+++ app/helpers/validate.php
@@ -1,17 +1,27 @@
 <?php
 
-function validate(array $validations)
+function validate(array $validations, bool $persistInputs = false, bool $checkCsrf = false)
 {
+    if ($checkCsrf) {
+        checkCsrf();
+    }
     $result = [];
-    $param =  '';
+    $param = '';
     foreach ($validations as $field => $validate) {
         $result[$field] = (!str_contains($validate, '|')) ?
             singleValidation($validate, $field, $param) :
             multipleValidations($validate, $field, $param);
     }
-    if (in_array(false, $result)) {
+
+
+    if ($persistInputs) {
+        setOld();
+    }
+
+    if (in_array(false, $result, true)) {
         return false;
     }
+
     return $result;
 }
 
@@ -31,8 +41,10 @@ function multipleValidations($validate, $field, $param)
         if (str_contains($validate, ':')) {
             [$validate, $param] = explode(':', $validate);
         }
+
         $result[$field] = $validate($field, $param);
-        if (isset($result[$field]) and $result[$field] === false) {
+
+        if ($result[$field] === false || $result[$field] === null) {
             break;
         }
     }
