72b324a4fa7aeeb3bced4bca2ee17a30abb90848
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2023-01-21 16:10:46 -0300

    teste

--- app/controllers/UserImage.php
+++ app/controllers/UserImage.php
@@ -6,6 +6,27 @@ class UserImage
 {
     public function store()
     {
-        dd($_FILES);
+        try {
+            $path = upload(640,480, 'assets/img', 'crop');
+            $auth = user();
+            read('photos');
+            where('userId', $auth->id);
+            $photoUser = execute(isFetchAll: false);
+            if ($photoUser) {
+                $updated = update('photos', [
+                    'path' => $path
+                ], [
+                    'userId' => $auth->id
+                ]);
+            } else {
+                $updated = create('photos', [
+                    'userId' => $auth->id,
+                    'path' => $path
+                ]);
+            }
+            dd($updated);
+        } catch (\Exception $e) {
+            setMessageAndRedirect('upload_error', $e->getMessage(), '/user/edit/profile');
+        }
     }
 }
--- app/views/edit.php
+++ app/views/edit.php
@@ -1,8 +1,9 @@
 <form action="/user/profile/update" method="post"></form>
 
 <hr>
+<?php echo getFlash('upload_error'); ?>
 <form action="/user/image/update" method="post" enctype="multipart/form-data">
-  <input type="file" name="file" accept="image/gif, image/jpeg, image/png" >
+  <input type="file" name="file"  >
   <button type="submit">Alterar foto</button>
 
 </form>
